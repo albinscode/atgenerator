771a506ba3a15cd0523d12638d7e95bb0b9366a0
albinscode <albinscode@gmail.com>
2016-10-27 14:02:42 +0200

    diff is now like other commands with full inquirer (console mode prompting)

--- commands/activity-diff.js
+++ commands/activity-diff.js
@@ -1,4 +1,5 @@
 var utils = require('../lib/Utils');
+var commandUtils = require('../lib/CommandUtils.js');
 
 // This is the sub command to generate the diff between time management and planning
 var program = require('commander');
@@ -7,22 +8,25 @@ program
     .option('-u --user <user>', 'user to connect to OBM service')
     .option('-p --password <password>', 'password to connect to OBM service')
     .option('-j --json <json>', 'json data to use for diff generation')
+    .option('-s --startDate <startDate>', 'the starting date')
+    .option('-e --endDate <endDate>', 'the ending date')
     .parse(process.argv);
 
-// Check of parameters not taken into account by commander.
-if (program.user === undefined) throw new Error("You must specify a user");
-if (program.password === undefined) throw new Error("You must specify a password");
-if (program.json === undefined) throw new Error("You must specify a json file");
+commandUtils.displayPrompt(program, [ 'user', 'password', 'json' ]).then(function(answers) {
+    performCommand();
+})
+.catch(function(reason) {
+    log.error('planning command', reason);
+});
 
-var fs = require('fs');
-var DiffGenerator = require('../lib/DiffGenerator');
+function performCommand() {
 
-var generator = new DiffGenerator();
+    var DiffGenerator = require('../lib/DiffGenerator');
 
-fs.readFile(program.json, function(err, content) {
+    var generator = new DiffGenerator();
     var json = utils.createJsonObject(program.json, program);
     var connectionProperties = { user: program.user, password: program.password, groupId: json.groupId };
 
     generator.generate(json, connectionProperties);
-});
+}
 
